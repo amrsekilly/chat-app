08fad79d769d14fa3fb5b9ab3b2a641132373b39
Amr Elsekilly <amr.e.dev@gmail.com>
2017-10-26 06:09:08 +0200

    detected user disconnection

--- index.js
+++ index.js
@@ -5,12 +5,16 @@ const http = require('http').Server(app);
 const Socket = require("socket.io")(http);
 
 // main route
-app.get('/',  (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"));
 
 // listen on port 3000 for requests
-http.listen(3000, () => console.log('listening on *:3000'));
+http.listen(3000, () => console.log("listening on *:3000"));
 
 // listen on any socket connection
 Socket.on('connection', (socket) => {
   console.log("A user is connected");
+  // detect if the client disconnected from the socket
+  socket.on('disconnect', () => {
+    console.log("user disconnected");
+  });
 });
