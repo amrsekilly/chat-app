7b622694d436816f05905787dac02d8418c6d83b
Amr Elsekilly <amr.e.dev@gmail.com>
2017-10-26 04:47:26 +0200

    refactored index.js

--- index.js
+++ index.js
@@ -9,11 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
+// listen on any socket connection
 Socket.on('connection', (socket) => {
   console.log("A user is connected");
 });
 
 // listen on port 3000 for requests
-http.listen(3000, function () {
-  console.log('listening on *:3000');
-});
+http.listen(3000, () => console.log('listening on *:3000'));
